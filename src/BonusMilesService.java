b9f5c8109641557d1bb281dacafb5e3fe093d7a3
Bella Khamchieva <bella.kh29@gmail.com>
2021-09-23 12:13:26 +0300

    BMS midified

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,8 @@
 public class BonusMilesService {
-    public int calculate(int cost) {
-        int miles = cost/20;
-        return miles;
-    }
+
+  public int calculate(int cost) {
+    int miles = cost / 20;
+    return miles;
+  }
+
 }
