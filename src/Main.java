a82c4c84b45bd2f7f61d5e60fae703fa93249b83
Bella Khamchieva <bella.kh29@gmail.com>
2021-09-23 12:14:11 +0300

    Main modified

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
-        int cost = 10_000;
-        int miles = service.calculate(cost);
-        System.out.println(miles);
-    }
+
+  public static void main(String[] args) {
+    BonusMilesService service = new BonusMilesService();
+    int cost = 10_000;
+    int miles = service.calculate(cost);
+    System.out.println(miles);
+  }
+
 }
